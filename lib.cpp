636fdfbfa3cf03cdb4b1c3c7441859392e644641
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-11-30 12:20:36 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#include "lib.h"
+#include "iostream"
+void primo (int a,int i,int &conta){
+    int div=0;
+    i++;
+    if (a>1 and a>i){
+        div= a%i;
+        if (div==0){
+            conta++;
+        }else {
+            primo (a,i,conta);
+        }
+    }
+}
