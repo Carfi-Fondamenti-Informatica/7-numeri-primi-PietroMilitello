2a92a6df9abd174a37a3621f022e0d129a0ee7da
PietroMilitello <91501751+PietroMilitello@users.noreply.github.com>
2021-11-30 12:19:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int numero=0, conta=0;
+    cin >> numero;
+
+    primo (numero, 1, conta);
+    if (conta==0){
+    cout << "numero primo"<< endl;
+} else {
+    cout << "numero non primo"<< endl;
+}
+    return 0;
 }
